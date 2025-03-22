b62680306b8201916f4e9a4e4991274ac4a0af40
Dusernajder <dusungulu.tibi@gmail.com>
2025-03-22 18:57:31 +0100

    advance, isEOF, addToken functions introduced, scanToken heavily modfied, still in progress

--- scanner/scanner.c
+++ scanner/scanner.c
@@ -1,21 +1,67 @@
+#include "scanner.h"
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include "../error/error.h"
+#include "../tokenizer/token.h"
 #include "../tokenizer/tokenizer.h"
 
-void scanTokens(char *source, TokenType tokensp[]) {
+extern bool hadError;
+
+size_t lineNumber = 1;
+size_t start = 0;
+size_t current = 0;
+
+void scanTokens(char *source, Token tokens_p[]) {
     size_t sourceLength = strlen(source);
-    for (int i = 0; i < sourceLength; i++) {
-        char *lexeme = (source + i);
-        /*if (lexeme == NULL || strcmp(lexeme, "\0")) {*/
-        /*    break;*/
-        /*}*/
+    size_t counter = 0;
+
+    while (!isEOF(current, sourceLength)) {
+
+        char *lexeme = malloc(current - start + 1);
+        lexeme = strncpy(lexeme, *(&source + start), current);
 
         printf("Lexeme: %s\n", lexeme);
 
         TokenType token = convertLexemeToToken(lexeme);
-        tokensp[i] = token;
+
+        if (token == UNDEFINED) {
+            hadError = true;
+            error(lineNumber, strcat("Undefined Lexeme: ", lexeme));
+            break;
+        }
+        else {
+            addToken(token, &tokens_p[counter], lexeme, lexeme);
+            start = current;
+            counter++;
+        }
+
+        /*tokens_p[lineNumber] = token;*/
+
+        free(lexeme);
+        advance(source, &current);
     }
+
+    /*tokens_p[lineNumber + 1] = EOF_;*/
+}
+
+char advance(char *source, size_t *current) {
+    return source[*current++];
+}
+
+bool isEOF(size_t current, size_t sourceLength) {
+    return current >= sourceLength;
+}
+
+void addToken(TokenType type, Token *tokenLocation_p, char *literal, char *lexeme) {
+    Token token = {
+        .type = type,
+        .lexeme = literal,
+        .literal = literal,
+        .line = lineNumber};
+
+    *tokenLocation_p = token;
 }
--- scanner/scanner.h
+++ scanner/scanner.h
@@ -1,10 +1,14 @@
 #ifndef SCANNER_H
 #define SCANNER_H
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 
-#include "../tokenizer/tokenizer.h"
+#include "../tokenizer/token.h"
 
-extern void scanTokens(char *, TokenType *);
+void scanTokens(char *, Token[]);
+char advance(char *, size_t *);
+bool isEOF(size_t, size_t);
+void addToken(TokenType, Token *, char *, char *);
 
 #endif
