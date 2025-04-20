ebfa44f3792862a51b6977f1f1c4971536ae6ac7
Dusernajder <dusungulu.tibi@gmail.com>
2025-04-20 12:58:59 +0200

    restructure, directory operations added to utils -> check for existence and create

---
+++ src/utils/utils.c
@@ -0,0 +1,29 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+#include "error/error.h"
+
+void *check_malloc(void *ptr) {
+    if (ptr == NULL) {
+        error(-1, "Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    return ptr;
+}
+
+bool is_directory_exists(char *directory) {
+    struct stat st;
+    return (stat(directory, &st) == 0) && S_ISDIR(st.st_mode);
+}
+
+void create_directory(char *directory) {
+    if (!is_directory_exists(directory)) {
+        if (mkdir(directory, 0755) != 0) {
+            perror("mkdir failed");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
---
+++ src/utils/utils.h
@@ -0,0 +1,6 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+void *check_malloc(char *);
+bool is_directory_exists(char *directory);
+void create_directory(char *directory);
