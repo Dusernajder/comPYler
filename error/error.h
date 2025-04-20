f2dd475b1b62c76c34b6f1d2c75e664aa672354c
Dusernajder <dusungulu.tibi@gmail.com>
2025-04-20 13:08:00 +0200

    files deleted after project restructure

--- error/error.h
+++
@@ -1,7 +0,0 @@
-#ifndef ERROR_H
-#define ERROR_H
-
-void error(int, char *);
-void report(int, char *, char *);
-
-#endif
--- utils/utils.h
+++
@@ -1,6 +0,0 @@
-#include <stdbool.h>
-#include <stdlib.h>
-
-#include "../error/error.h"
-
-void *check_malloc(char *);
