e80c7f0a97164775468724f1a5efe7eb4ee21443
Dusernajder <dusungulu.tibi@gmail.com>
2025-05-07 16:21:43 +0200

    removed Expression, no need for it anymore

--- src/tool/ast.h
+++ src/tool/ast.h
@@ -4,18 +4,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-#include "token/token.h"
-
-typedef struct Expression Expression;
-
-typedef struct Expression {
-    Expression *left_p;
-    Token operator_;
-    Expression *right_p;
-} Expression;
-
 void define_ast(char *output_dir, char *file_name, size_t definitions_length, char *definitions[definitions_length]);
-void define_struct(FILE *header_file_p, char *struct_name, char *field_names);
+void define_expression(FILE *header_file_p, char *struct_name, char *field_names);
 void fprintf_ln(FILE *file, const char *format, ...);
 
 #endif
