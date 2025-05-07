a5bc42b9a27372c4e9c6aeca7c21eade6a46ddb0
Dusernajder <dusungulu.tibi@gmail.com>
2025-05-07 16:22:55 +0200

    removed nonsense, restructured generated code to use a union

--- src/tool/ast.c
+++ src/tool/ast.c
@@ -9,7 +9,7 @@
 #include "utils/utils.h"
 
 void define_ast(char *output_dir, char *file_name, size_t definitions_length, char *definitions[definitions_length]) {
-    size_t path_length = strlen(output_dir) + strlen(file_name) + 4; // +4 = folder separator, null terminator and file extension
+    size_t path_length = strlen(output_dir) + strlen(file_name) + 4; // +4 = folder separator, file extension and null terminator
     char *header_path = (char *)check_malloc(malloc(path_length * sizeof(char)));
     char *source_path = (char *)check_malloc(malloc(path_length * sizeof(char)));
 
@@ -35,34 +35,40 @@ void define_ast(char *output_dir, char *file_name, size_t definitions_length, ch
     fprintf_ln(header_file_p, "#define EXPRESSION_H");
 
     fprintf_ln(header_file_p, "#include \"../src/token/token.h\"");
-    fprintf_ln(header_file_p, "#include \"../src/tool/ast.h\"");
     fprintf_ln(header_file_p, "#include \"../src/tool/py_type/py_type.h\"");
     fprintf_ln(header_file_p, "");
+
+    fprintf_ln(header_file_p, "typedef struct Expression Expression;");
+
+    fprintf_ln(header_file_p, "typedef struct Expression {");
+
+    fprintf_ln(header_file_p, "\tunion {");
 
     // ast structs
     for (unsigned int i = 0; i < definitions_length; i++) {
         char *definition = strdup(definitions[i]);
         char *struct_name = strtok(definition, " :");
         char *field_names = strtok(NULL, ":");
-        define_struct(header_file_p, struct_name, field_names);
+        define_expression(header_file_p, struct_name, field_names);
 
         free(definition);
     }
 
+    fprintf_ln(header_file_p, "\t};");
+    fprintf_ln(header_file_p, "} Expression;");
+
+    fprintf_ln(header_file_p, "");
+
     fprintf_ln(header_file_p, "#endif");
 
-    // source
-    fprintf_ln(source_file_p, "#include \"%s.h\"", file_name);
 
     fclose(header_file_p);
-    fclose(source_file_p);
 
     free(header_path);
-    free(source_path);
 }
 
-void define_struct(FILE *header_file_p, char *struct_name, char *field_names) {
-    fprintf_ln(header_file_p, "typedef struct %s {", struct_name);
+void define_expression(FILE *header_file_p, char *struct_name, char *field_names) {
+    fprintf_ln(header_file_p, "\t\tstruct {");
 
     char *save_field_ptr;
     char *field = strtok_r(field_names, ",", &save_field_ptr);
@@ -71,16 +77,25 @@ void define_struct(FILE *header_file_p, char *struct_name, char *field_names) {
         char *member = strdup(field);
         char *type = strtok(member, " ");
         char *identifier = strtok(NULL, " ");
+        
+        char *identifier_pointer = check_malloc(malloc(sizeof(identifier) + 2));
 
-        fprintf_ln(header_file_p, "\t%s %s;", type, identifier);
+        bool is_identifier_pointer = !strcmp(type, "Expression");
+
+        if (is_identifier_pointer){
+            identifier_pointer = concat_str("*", identifier);
+        }
+
+        char *result = is_identifier_pointer ? identifier_pointer : identifier;
+        fprintf_ln(header_file_p, "\t\t\t%s %s;", type, result);
 
         field = strtok_r(NULL, ",", &save_field_ptr);
 
+        free(identifier_pointer);
         free(member);
     }
 
-    fprintf_ln(header_file_p, "} %s;", struct_name);
-    fprintf_ln(header_file_p, "");
+    fprintf_ln(header_file_p, "\t\t} %s;", struct_name);
 
     free(field);
 }
